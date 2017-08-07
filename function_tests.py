042b62ab9ed254a970fb7e42e3a730fc039769ce
xiaotiao <34100102@qq.com>
2017-08-07 15:14:00 +0800

    Front page HTML now generated from a template

--- function_tests.py
+++ function_tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
 
         #应用邀请她输入一个待办事项
-        inputbox=self.browser.find_element_by_tag_id('id_new_item')
+        inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -43,7 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text=='1:Buy peacock feathers' for row in rows)
+            any(row.text=='1:Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
             )
 
         #页面又显示了一个文本框，可以输入其他的待办事项
