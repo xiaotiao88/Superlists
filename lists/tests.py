857a6c1d4f24a4d9e8d7c22406536ba96e696c0d
xiaotiao <34100102@qq.com>
2017-08-08 11:54:39 +0800

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,10 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
 from django.template.loader import render_to_string
+
 
 import re
 
@@ -44,4 +46,23 @@ class HomePageTest(TestCase):
         expected_html = re.sub(csrf_regex, '', expected_html)
         self.assertEqual(observed_html,expected_html)
 #        self.assertEqual(response.content.decode(),expected_html)
+        
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text='The first (ever) list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'Item the second')
         
