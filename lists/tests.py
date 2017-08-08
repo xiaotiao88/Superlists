aacee1f9fc601a1839b246aa569ad5eb3d1319e3
xiaotiao <34100102@qq.com>
2017-08-08 10:26:32 +0800

    commit 20170808

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,8 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 
+import re
+
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -13,11 +15,33 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func,home_page)
 
     def test_home_page_returns_correct_html(self):
-        request=HttpRequest()
-        response=home_page(request)
-        expected_html=render_to_string('home.html')
-        self.assertEqual(response.content.decode(),expected_html)
+         request=HttpRequest()
+         response=home_page(request)
+         expected_html=render_to_string('home.html')
+#         self.assertEqual(response.content.decode(),expected_html)
 #        self.assertTrue(response.content.startswith(b'<html>'))
 #        self.assertIn(b'<title>To-Do lists</title>',response.content)
 #        self.assertTrue(response.content.endswith(b'</html>'))
+         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+         observed_html = re.sub(csrf_regex, '', response.content.decode())
+         expected_html = re.sub(csrf_regex, '', expected_html)
+         self.assertEqual(observed_html,expected_html)
     
+
+    def test_home_page_can_save_a_POST_request(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+
+        response=home_page(request)
+        self.assertIn('A new list item',response.content.decode())
+        expected_html=render_to_string(
+            'home.html',
+            {'new_item_text':'A new list item'}
+            )
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        observed_html = re.sub(csrf_regex, '', response.content.decode())
+        expected_html = re.sub(csrf_regex, '', expected_html)
+        self.assertEqual(observed_html,expected_html)
+#        self.assertEqual(response.content.decode(),expected_html)
+        
