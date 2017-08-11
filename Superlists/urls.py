486c31ee3d3965041499b190ea3725c26fbaf46e
xiaotiao <34100102@qq.com>
2017-08-11 11:15:21 +0800

    20170811

--- Superlists/urls.py
+++ Superlists/urls.py
@@ -13,20 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from lists.views import home_page
-from lists.views import view_list
-from lists.views import new_list
-from lists.views import add_item
+import lists
+from lists import urls
+# from lists.views import view_list
+# from lists.views import new_list
+# from lists.views import add_item
 
 
 urlpatterns =[
     # Examples:
      url(r'^$',home_page,name='home'),
-     url(r'^lists/(\d+)/$',view_list,name='view_list'),
-     url(r'^lists/(\d+)/add_item$',add_item,name='add_item'),
-     url(r'^lists/new$',new_list,name='new_list'),    
+     url(r'^lists/',include(lists.urls)),
+     # 以下三行由上面一行及lists/urls.py代替
+     # url(r'^lists/(\d+)/$',view_list,name='view_list'),
+     # url(r'^lists/(\d+)/add_item$',add_item,name='add_item'),
+     # url(r'^lists/new$',new_list,name='new_list'),    
     # url(r'^blog/',include('blog.urls')),
     
     # url(r'^admin/', include(admin.site.urls)),
